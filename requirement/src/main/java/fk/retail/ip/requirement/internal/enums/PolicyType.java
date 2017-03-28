0ee0b571baa7d860718db05018e636d6bb282eac
Nidhi Gupta <nidhsg16@gmail.com>
2017-03-28 15:29:56 +0530

    null check in search filetr

--- requirement/src/main/java/fk/retail/ip/requirement/internal/enums/PolicyType.java
+++ requirement/src/main/java/fk/retail/ip/requirement/internal/enums/PolicyType.java
@@ -21,7 +21,7 @@ public enum PolicyType {
     public static PolicyType fromString(String value) {
         for (PolicyType type : values()) {
             if (type.value.equals(value)) {
-                return type;
+                return type;r
             }
         }
         log.warn(Constants.INVALID_POLICY_TYPE, value);
